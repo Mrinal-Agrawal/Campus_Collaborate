b4aafef4a5e558eb37c979127437677880b7788a
Shreeya1904 <shreeya1904@gmail.com>
2024-02-10 23:14:08 +0530

    frontend

---
+++ Frontend/Login.js
@@ -0,0 +1,6 @@
+function details(){
+    let userName=document.getElementById('user_name');
+    let RollNo=document.getElementById('RollNo');
+    let outlookid=document.getElementById('Email_id');
+    let phoneNo=document.getElementById('phone_number');
+}
